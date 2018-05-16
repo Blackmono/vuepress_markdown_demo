6c39e76b928e323c91ece5cd57371eea09fb8315
Miriam Suzanne <miriam@oddbird.net>
2018-05-16 10:20:01 -0600

    Sample un-rendered title and excerpt

---
+++ .vuepress/theme/Layout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app-root">
+    <main>
+      <p class="meta">This content will be rendered:</p>
+      <Content/>
+    </main>
+    <aside>
+      <p class="meta">This content will not:</p>
+      <article v-for="page in this.$site.pages">
+        <div>{{ page.title }}</div>
+        <div>{{ page.excerpt }}</div>
+      </article>
+    </aside>
+  </div>
+</template>
+
+<style>
+.meta {
+  background: black;
+  color: white;
+  margin: 1em 0;
+  padding: 1em;
+}
+</style>
+
---
+++ README.md
@@ -0,0 +1,8 @@
+# *VuePress* Markdown [Header](#@@@)
+
+*This* is an [excerpt with markdown](#@@@).
+
+<!-- more -->
+
+*This* is [the full content](#@@@)
+also using **markdown**.
